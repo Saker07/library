96922f4b005c751d3b65c1563832b214364f1e24
saker <gjoni.klaudio@gmail.com>
2022-08-01 11:53:55 +0100

    Finish supporting functions, add to do list

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # library
+
+Todo:
+    id cannot be just numbers, it creates problems with the css selectors, rather than taht use the data attribute.
+    delete book function
+    
--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let library = [];
 let el;
 el = document.querySelector(".add");
-el.addEventListener("click", addBookToLibrary)
+el.addEventListener("click", addBookToLibrary);
+el.addEventListener("click", displayLibrary);
+
+/*  */
 let ex = new Book("title", "author", "5", true);
 library.push(ex);
-let a = document.querySelector(".bookEdit");
-a.addEventListener("click", editBook, {capture:true})
+/*  */
+let a = document.querySelectorAll(".bookEdit");
+a.forEach(elem => elem.addEventListener("click", editBook, {capture:true}));
+
 
 function Book (title, author, pages, read){
     this.title = title;
@@ -23,6 +28,7 @@ function addBookToLibrary(e){
     read = document.querySelector("#read").value;
     book = new Book(title, author, pages, read);
     library.push(book);
+    el.addEventListener("click", resetForm);
 }
 
 function editBook(e){
@@ -39,14 +45,45 @@ function editBook(e){
     pages.value = library[book].pages;
     read.value = library[book].read;
     editB.textContent = "Edit";
+    editB.removeEventListener("click", addBookToLibrary);
+    editB.addEventListener("click", editBookToLibrary);
+}
+function editBookToLibrary(i){
+    let book;
+    let title, author, pages, read;
+    let editB;
+    editB = document.querySelector(".add");
+    title = document.querySelector("#title").value;
+    author = document.querySelector("#author").value;
+    pages = document.querySelector("#pages").value;
+    read = document.querySelector("#read").value;
+    book = new Book(title, author, pages, read);
+    library.splice(i, 1, book);
+    editB.textContent = "Add";
+    editB.removeEventListener("click", editBookToLibrary)
+    el.addEventListener("click", resetForm);
+}
+function resetForm(){
+    let em= document.querySelectorAll("form input");
+    em.forEach(elem => elem.value="")
 }
 
-function displatLibrary(){
-    
+function displayLibrary(){
+    let cont, elem;
+    cont = document.querySelector(".libraryContainer");
+    cont.innerHTML = "";
+    console.log(cont);
+    for(let i = 0; i<library.length; i++){
+        elem = createBookElement(library[i], i);
+        console.log(elem);
+        cont.appendChild(elem);
+    }
+    let a = document.querySelectorAll(".bookEdit");
+    a.forEach(elem => elem.addEventListener("click", editBook, {capture:true}));
 }
 
 function createBookElement(book, i){
-    let el;
+    let el, el2;
     let item = document.createElement("div");
     item.classList.add("book");
     item.id = i;
@@ -69,12 +106,16 @@ function createBookElement(book, i){
     el = document.createElement("button");
     el.classList.add("bookButton", "bookEdit");
     el.setAttribute("type", "button");
-    el.textContent = "Edit";
+    el2 = document.createElement("h6")
+    el2.textContent = "Edit";
+    el.appendChild(el2);
     item.appendChild(el);
     el = document.createElement("button");
     el.classList.add("bookButton", "bookDelete");
     el.setAttribute("type", "button");
-    el.textContent = "Delete";
+    el2 = document.createElement("h6")
+    el2.textContent = "Delete";
+    el.appendChild(el2);
     item.appendChild(el);
     return item;
 }
